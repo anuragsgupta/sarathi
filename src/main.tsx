a7a86fa58c8c86c8fe5d0b56fa6ef20221cdb1d1
anuragsgupta <anuragsgupta9211@gmail.com>
2024-12-25 11:11:00 +0530

    remove dist from gitingnore

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter basename={"/vite-react-ts/"}>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
