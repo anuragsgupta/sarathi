b189e11c3ccd69df2ffaf1b091958d0ad7a694f2
anuragsgupta <anuragsgupta9211@gmail.com>
2024-12-30 15:30:55 +0530

    change root /sarathi/ to /

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <BrowserRouter basename={"/sarathi/"}>
+      <BrowserRouter basename={"/"}>
         <App />
       </BrowserRouter>
     </StrictMode>
